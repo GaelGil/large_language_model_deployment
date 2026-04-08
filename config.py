ec08fea8960c9406cf24e31aa7d180d903f7b683
Gael Gil <gaelgilbello27@gmail.com>
2026-04-07 18:43:12 -0700

    created basic layout

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config(BaseModel):
     SRC_CORPUS_PATH: str
     TOKENIZER_PATH: str
     SEQ_LEN: int
+    MODEL_PATH: str
 
 
-CONFIG = Config(SRC_CORPUS_PATH="", TOKENIZER_PATH="", SEQ_LEN=1)
+CONFIG = Config(SRC_CORPUS_PATH="", TOKENIZER_PATH="", SEQ_LEN=1, MODEL_PATH="")
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from pathlib import Path
-import sentencepiece as spm
 import modal
+import sentencepiece as spm
+
 from config import CONFIG
+
 app = modal.App("seq2seq-jax-inference")
 
 image = modal.Image.debian_slim().uv_pip_install(
@@ -9,27 +10,24 @@ image = modal.Image.debian_slim().uv_pip_install(
     "flax",
     "numpy",
     "orbax-checkpoint",
-    "sentencepiece",  # if you use it
+    "sentencepiece",
 )
 
 model_volume = modal.Volume.from_name("seq2seq-model-weights", create_if_missing=True)
-MODEL_DIR = "/models"
 
 
 @app.cls(
     image=image,
-    volumes={MODEL_DIR: model_volume},
+    volumes={CONFIG.MODEL_PATH: model_volume},
     cpu=4.0,
     memory=8192,
 )
 class Seq2SeqModel:
     @modal.enter()
     def load(self):
-        self.model_path = Path(MODEL_DIR) / "my_checkpoint"
-
-        self.tokenizer = spm.SentencePieceProcessor().Load(f"{CONFIG.SRC_CORPUS_PATH}/{config.model_prefix}.model")config.SEQ_LEN,)
-
-
+        self.model_path = CONFIG.MODEL_PATH
+        sp = spm.SentencePieceProcessor()
+        self.tokenizer = sp.Load(CONFIG.MODEL_PATH)
 
     @modal.method()
     def predict(self, text: str, max_new_tokens: int = 128) -> dict:
